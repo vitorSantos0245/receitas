8a736bb764c2bc2f9059d264c266802ba0306b54
VITOR SANTOS PEREIRA <vitor_pereira153@estudante.sesisenai.org.br>
2025-09-24 17:02:21 -0300

    ts

---
+++ App.js
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import ADICIONAR from './AdicionarLista';
+import VerificarLista from './verLista';
+
+export default function App() {
+
+  let [pagina, setPagina] = useState('ver');
+
+  const navegacao = (rota) => {
+    setPagina(rota);
+  };
+
+  const paginas = () => {
+    switch (pagina) {
+      case 'add':
+        return <ADICIONAR navegacao={navegacao} />;
+        case 'ver':
+          return <VerificarLista navegacao={navegacao} />
+      default:
+        return <VerificarLista navegacao={navegacao} />;
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+    <Text>NÃO PODE ESTAR NESSA PÁGINA</Text>
+    {paginas}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: '50',
+    padding: '40'
+  },
+  inputs: {
+    backgroundColor: 'lightblue'
+  },
+  botao: {
+    backgroundColor: 'yellow'
+  },
+  textos: {
+    padding: '50px',
+    margin: '30px',
+  }
+});
